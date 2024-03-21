0dc12f4d3b64376513a4e7fa6a488509004ce51e
T-Bone <99473865+T-BonesLek@users.noreply.github.com>
2024-03-21 17:34:50 +0100

    Delete src directory

--- src/pymoveit2/pymoveit2/robots/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import ur5
-from . import ur5_sim
--- src/pymoveit2/textilePrograms/killANodes.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-# Get the PID of the main ROS2 process
-pid = subprocess.check_output(['pgrep', '-f', 'ros2']).decode().strip()
-
-# Kill the process
-subprocess.run(['kill', '-SIGINT', pid])
